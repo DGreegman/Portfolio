ba9b78af96abc93b096fe8287008e7c0d6746d1f
DGreegman <obeagug@gmail.com>
2023-04-23 22:49:02 +1300

    First Commit of my site

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiplied-text", {
+    strings: ["Backend Developer", "Content Creator", "Instrumentalist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
